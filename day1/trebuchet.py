e2944e2ec85184eed02cd5eb3973fba9786f7bd5
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2023-12-01 09:22:07 +0100

    day 1 part 2

--- day1/trebuchet.py
+++ day1/trebuchet.py
@@ -1,7 +1,22 @@
+import typing
+
 FILENAME: str = "input.txt"
 
+NUMBERS: typing.Dict[str, int] = {
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
 
-def main():
+
+def part1() -> None:
     with open(FILENAME, "r") as fh:
         lines = fh.readlines()
 
@@ -10,5 +25,26 @@ def main():
     print(answer)
 
 
+def part2() -> None:
+    with open(FILENAME, "r") as fh:
+        lines = fh.readlines()
+
+    digits: typing.List[typing.List[int]] = []
+    for line in lines:
+        ld = []
+        for pos, c in enumerate(line):
+            if c.isdigit():
+                ld.append(int(c))
+            else:
+                for word, value in NUMBERS.items():
+                    if line[pos : pos + len(word)] == word:
+                        ld.append(value)
+
+        digits.append(ld)
+
+    answer = sum([d[0] * 10 + d[-1] for d in digits])
+    print(answer)
+
+
 if __name__ == "__main__":
-    main()
+    part2()

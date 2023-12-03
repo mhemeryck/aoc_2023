cea987a265f2420cf29230fe72c97201f8a455b4
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2023-12-04 00:04:17 +0100

    day3, part deux, first attempt

--- day3/gear_ratios.py
+++ day3/gear_ratios.py
@@ -2,6 +2,8 @@ import typing
 
 FILENAME: str = "input.txt"
 
+OFFSETS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
 
 def is_symbol(el: str) -> bool:
     return el != "." and not el.isdigit()
@@ -10,8 +12,7 @@ def is_symbol(el: str) -> bool:
 def neighbor_has_symbol(lines: typing.List[str], i: int, j: int, max_i: int, max_j: int) -> bool:
     indices: typing.List[typing.Tuple[int, int]] = []
 
-    offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-    for o_i, o_j in offsets:
+    for o_i, o_j in OFFSETS:
         x, y = (i + o_i), (j + o_j)
         if 0 <= x < max_i and 0 <= y < max_j:
             indices.append((x, y))
@@ -19,7 +20,52 @@ def neighbor_has_symbol(lines: typing.List[str], i: int, j: int, max_i: int, max
     return any(is_symbol(lines[i][j]) for i, j in indices)
 
 
-def main() -> None:
+def find_neighboring_numbers(lines: typing.List[str], i: int, j: int, max_i: int, max_j: int) -> typing.List[int]:
+    # coords that hava already been checked
+    checked: typing.List[typing.Tuple[int, int]] = []
+
+    indices: typing.List[typing.Tuple[int, int]] = []
+    for o_i, o_j in OFFSETS:
+        x, y = (i + o_i), (j + o_j)
+        if 0 <= x < max_i and 0 <= y < max_j:
+            indices.append((x, y))
+
+    numbers = []
+    # Check all candidates
+    for x, y in indices:
+        # Check the candidate itself
+        number_str = ""
+        if lines[x][y].isdigit() and (x, y) not in checked:
+            number_str += lines[x][y]
+        else:
+            continue
+        checked.append((x, y))
+
+        # grow right
+        for a in range(y + 1, max_j):
+            can = lines[x][a]
+            if can.isdigit() and (x, a) not in checked:
+                number_str += can
+                checked.append((x, a))
+            else:
+                break
+        # grow left
+        for a in range(y - 1, 0, -1):
+            can = lines[x][a]
+            if can.isdigit() and (x, a) not in checked:
+                # prepend in case of left grow
+                number_str = f"{can}{number_str}"
+                checked.append((x, a))
+            else:
+                break
+
+        if number_str:
+            numbers.append(int(number_str))
+
+    return numbers
+
+
+def part1() -> None:
     with open(FILENAME, "r") as fh:
         lines = fh.read().splitlines()
 
@@ -50,6 +96,20 @@ def main() -> None:
             result += int(number_str)
 
     print(result)
+
+
+def main() -> None:
+    with open(FILENAME, "r") as fh:
+        lines = fh.read().splitlines()
+
+    max_i = len(lines)
+    result = 0
+    for i, line in enumerate(lines):
+        max_j = len(line)
+        for j, el in enumerate(line):
+            if el == "*":
+                idx = find_neighboring_numbers(lines, i, j, max_i, max_j)
+                print(idx)
 
 
 if __name__ == "__main__":

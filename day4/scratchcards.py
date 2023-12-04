3cf3cab815a87cd1c9983c6edfe628d082b43094
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2023-12-04 13:06:42 +0100

    part2

--- day4/scratchcards.py
+++ day4/scratchcards.py
@@ -1,3 +1,6 @@
+import re
+import typing
+
 with open("input.txt") as fh:
     LINES = fh.readlines()
 
@@ -10,25 +13,65 @@ with open("input.txt") as fh:
 # Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11""".splitlines()
 
 
-def main() -> None:
+def part1() -> None:
     result = 0
     for line in LINES:
-        game_id, payload = line.split(":")
+        game_id_str, payload = line.split(":")
+        if m := re.match(r"Card\s+(\d+)", game_id_str):
+            game_id = int(m.group(1))
+        else:
+            game_id = -1
+
         # parsing
         winning, actual = payload.split("|")
         winning = [int(n) for n in winning.split(" ") if n]
         actual = [int(n) for n in actual.split(" ") if n]
 
-        line_count = 0
-        for w in winning:
-            if w in actual:
-                line_count += 1
-
+        line_count = sum(w in actual for w in winning)
         if line_count:
             result += 1 << (line_count - 1)
 
         print(game_id, winning, actual, line_count, result)
     print(result)
+
+
+def main() -> None:
+    result = 0
+    # map of game id with line count
+    line_counts: typing.Dict[int, int] = {}
+    for line in LINES:
+        game_id_str, payload = line.split(":")
+        if m := re.match(r"Card\s+(\d+)", game_id_str):
+            game_id = int(m.group(1))
+        else:
+            game_id = -1
+
+        # parsing
+        winning, actual = payload.split("|")
+        winning = [int(n) for n in winning.split(" ") if n]
+        actual = [int(n) for n in actual.split(" ") if n]
+
+        line_count = sum(w in actual for w in winning)
+        line_counts[game_id] = line_count
+
+        print(game_id, winning, actual, line_count, result)
+    # print(result)
+    # print(line_counts)
+
+    max_game_id = max(line_counts.keys())
+
+    # Counts the number cards for a game card
+    # Init with all game ids having one card
+    counter: typing.Dict[int, int] = {game_id: 1 for game_id in line_counts.keys()}
+    for game_id, line_count in line_counts.items():
+        fro = game_id + 1
+        to = game_id + line_count + 1
+
+        for next_id in range(fro, to):
+            counter[next_id] += counter[game_id]
+
+    print(counter)
+    print(sum(counter.values()))
 
 
 if __name__ == "__main__":

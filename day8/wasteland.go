9dbcbaaced5d32a9b0b86b1cddb9f956ac4b18aa
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2023-12-12 00:13:29 +0100

    wasteland, part2

--- day8/wasteland.go
+++ day8/wasteland.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"regexp"
+	"slices"
 	"strings"
 )
 
@@ -11,6 +12,7 @@ const (
 	FILENAME = "input.txt"
 	// FILENAME = "example.txt"
 	// FILENAME = "example2.txt"
+	// FILENAME = "example3.txt"
 )
 
 var (
@@ -19,11 +21,51 @@ var (
 	END     = Point("ZZZ")
 )
 
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
+
 type Point string
 
 type Edge struct {
 	Left  Point
 	Right Point
+}
+
+func countHops(start Point, endPoints []Point, points map[Point]Edge, directions string) int {
+	count := 0
+	var point Point = start
+	for !slices.Contains(endPoints, point) {
+		for _, d := range directions {
+			if string(d) == "L" {
+				point = points[point].Left
+			} else if string(d) == "R" {
+				point = points[point].Right
+			} else {
+				panic("unexpected direction")
+			}
+			// fmt.Printf("-> %v ", point)
+			count++
+		}
+	}
+	fmt.Printf("We've hit an endpoint! %v\n", point)
+	return count
 }
 
 func main() {
@@ -51,19 +93,42 @@ func main() {
 	}
 	// fmt.Printf("points %v\n", points)
 
-	count := 0
-	var point = START
-	for point != END {
-		for _, d := range directions {
-			if string(d) == "L" {
-				point = points[point].Left
-			} else if string(d) == "R" {
-				point = points[point].Right
-			} else {
-				panic("unexpected direction")
-			}
-			count++
+	// count := 0
+	// var point = START
+	// for point != END {
+	// 	for _, d := range directions {
+	// 		if string(d) == "L" {
+	// 			point = points[point].Left
+	// 		} else if string(d) == "R" {
+	// 			point = points[point].Right
+	// 		} else {
+	// 			panic("unexpected direction")
+	// 		}
+	// 		count++
+	// 	}
+	// }
+	// count := countHops(START, []Point{END}, points, directions)
+	// fmt.Printf("part 1: %v\n", count)
+
+	// part2
+	startNodes := make([]Point, 0)
+	endNodes := make([]Point, 0)
+	for key := range points {
+		if strings.HasSuffix(string(key), "A") {
+			startNodes = append(startNodes, key)
+		} else if strings.HasSuffix(string(key), "Z") {
+			endNodes = append(endNodes, key)
 		}
 	}
-	fmt.Printf("part 1: %v\n", count)
+
+	counts := make([]int, len(startNodes))
+	for k, point := range startNodes {
+		fmt.Printf("start node: %v\n", point)
+		counts[k] = countHops(point, endNodes, points, directions)
+
+	}
+
+	fmt.Printf("count  %v\n", counts)
+	result := LCM(counts[0], counts[1], counts[2:]...)
+	fmt.Printf("part 2: %v\n", result)
 }

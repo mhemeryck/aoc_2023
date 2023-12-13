c232d0e0e98998d6a1d98e3db62eadf3c771f005
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2023-12-13 23:32:04 +0100

    adapted bound check

--- day10/maze.py
+++ day10/maze.py
@@ -1,3 +1,4 @@
+import typing
 import numpy as np
 
 DATA = """.....
@@ -12,9 +13,13 @@ SJ.L7
 |F--J
 LJ..."""
 
-# FILENAME: str = "input.txt"
-# with open(FILENAME, "r") as fh:
-#     DATA = fh.read()
+FILENAME: str = "input.txt"
+with open(FILENAME, "r") as fh:
+    DATA = fh.read()
+
+
+def bound_check(x: int, y: int, x_max: int, y_max: int) -> bool:
+    return 0 <= x < x_max and 0 <= y < y_max
 
 
 def main() -> None:
@@ -33,45 +38,40 @@ def main() -> None:
         can = counts[can_idx]
         # north
         north_idx = can_idx[0] - 1, can_idx[1]
-        try:
+        if bound_check(north_idx[0], north_idx[1], maze.shape[0], maze.shape[1]):
             north = maze[north_idx]
-        except IndexError:
-            pass
-        else:
             if (counts[north_idx] == 0 or counts[north_idx] > can) and north in ("7", "F", "|"):
                 counts[north_idx] = can + 1
                 candidates.append(north_idx)
         # south
         south_idx = can_idx[0] + 1, can_idx[1]
-        try:
+        if bound_check(south_idx[0], south_idx[1], maze.shape[0], maze.shape[1]):
             south = maze[south_idx]
-        except IndexError:
-            pass
-        else:
             if (counts[south_idx] == 0 or counts[south_idx] > can) and south in ("J", "L", "|"):
                 counts[south_idx] = can + 1
                 candidates.append(south_idx)
         # east
         east_idx = can_idx[0], can_idx[1] + 1
-        try:
+        if bound_check(east_idx[0], east_idx[1], maze.shape[0], maze.shape[1]):
             east = maze[east_idx]
-        except IndexError:
-            pass
-        else:
             if (counts[east_idx] == 0 or counts[east_idx] > can) and east in ("J", "7", "-"):
                 counts[east_idx] = can + 1
                 candidates.append(east_idx)
         # west
         west_idx = can_idx[0], can_idx[1] - 1
-        try:
+        if bound_check(west_idx[0], west_idx[1], maze.shape[0], maze.shape[1]):
             west = maze[west_idx]
-        except IndexError:
-            pass
-        else:
             if (counts[west_idx] == 0 or counts[west_idx] > can) and west == ("L", "F", "-"):
                 counts[west_idx] = can + 1
                 candidates.append(west_idx)
-        print(counts)
+        breakpoint()
+        print(
+            counts[
+                np.max([can_idx[0] - 10, 0]) : np.min([can_idx[0] + 11, counts.shape[0] - 1]),
+                np.max([can_idx[1] - 10, 0]) : np.min([can_idx[1] + 11, counts.shape[1] - 1]),
+            ]
+        )
+        # print(counts)
 
     result = np.max(counts)
     print("part 1: max value", result)

1961da501d99309c018047fe4769a8335b66e2f7
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2023-12-14 00:04:48 +0100

    day 10, part 2 example

--- day10/maze.py
+++ day10/maze.py
@@ -22,6 +22,18 @@ with open(FILENAME, "r") as fh:
     DATA = fh.read()
 
 
+DATA = """FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L"""
+
+
 def bound_check(x: int, y: int, x_max: int, y_max: int) -> bool:
     return 0 <= x < x_max and 0 <= y < y_max
 
